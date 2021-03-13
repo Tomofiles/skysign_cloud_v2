6edd8dce64014aa32dc431b43235773c3bfc9aa8
tomofiles <tomofiles.ssd.1@gmail.com>
2021-03-13 22:12:34 +0900

    ドメインイベントのルール統一化による、関連機能の対応（vehicle）

--- communication/src/test/java/net/tomofiles/skysign/vehicle/api/EventObjectMother.java
+++ communication/src/test/java/net/tomofiles/skysign/vehicle/api/EventObjectMother.java
@@ -7,8 +7,9 @@ public class EventObjectMother {
     /**
      * テスト用VehicleCopiedWhenCopiedEventのProtocolBuffersバイナリデータを生成する。
      */
-    public static byte[] newNormalVehicleCopiedWhenCopiedEvent(VehicleId originalId, VehicleId newId) {
-        return proto.skysign.event.VehicleCopiedWhenCopiedEvent.newBuilder()
+    public static byte[] newNormalVehicleCopiedWhenCopiedEvent(String flightplanId, VehicleId originalId, VehicleId newId) {
+        return proto.skysign.event.VehicleCopiedWhenFlightplanCopiedEvent.newBuilder()
+            .setFlightplanId(flightplanId)
             .setOriginalVehicleId(originalId.getId())
             .setNewVehicleId(newId.getId())
             .build()
