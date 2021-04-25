3b8396de1ade8cb6045165a1bcf8d26d49b16a67
tomofiles <tomofiles.ssd.1@gmail.com>
2021-04-25 17:12:44 +0900

    テーブル定義を最新化（communicationの省略をフルスペルにする対応）

--- skysign-db/vehicle-db/vehicle.sql
+++ skysign-db/vehicle-db/vehicle.sql
@@ -1,7 +1,7 @@
 CREATE TABLE vehicle (
     id character varying(36) NOT NULL,
     name character varying(200) NOT NULL,
-    comm_id character varying(36) NOT NULL,
+    communication_id character varying(36) NOT NULL,
     is_carbon_copy boolean NOT NULL,
     version character varying(36) NOT NULL,
     CONSTRAINT vehicle_pkey PRIMARY KEY (id)
