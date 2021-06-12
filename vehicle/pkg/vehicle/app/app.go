f73f335d7729170a9e491640bb98104bf7b64cc3
tomofiles <tomofiles.ssd.1@gmail.com>
2021-06-12 19:19:37 +0900

    vehicleのgoプロジェクトをベースを新規作成

---
+++ vehicle/pkg/vehicle/app/app.go
@@ -0,0 +1,13 @@
+package app
+
+// Application .
+type Application struct {
+	Services Services
+}
+
+// Services .
+type Services struct {
+	// ManageFlightplan service.ManageFlightplanService
+	// ManageFleet      service.ManageFleetService
+	// AssignFleet      service.AssignFleetService
+}
---
+++ vehicle/pkg/vehicle/app/register.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"context"
+	"vehicle/pkg/vehicle/domain/event"
+	"vehicle/pkg/vehicle/domain/txmanager"
+)
+
+// NewApplication .
+func NewApplication(
+	ctx context.Context,
+	txm txmanager.TransactionManager,
+	psm event.PubSubManager,
+) Application {
+	return newApplication(ctx, txm, psm)
+}
+
+func newApplication(
+	ctx context.Context,
+	txm txmanager.TransactionManager,
+	psm event.PubSubManager,
+) Application {
+	// flightplanGen := uuid.NewFlightplanUUID()
+	// fleetGen := uuid.NewFleetUUID()
+	// flightplanRepo := postgresql.NewFlightplanRepository(flightplanGen)
+	// fleetRepo := postgresql.NewFleetRepository(fleetGen)
+	return Application{
+		Services: Services{
+			// ManageFlightplan: service.NewManageFlightplanService(flightplanGen, flightplanRepo, txm, psm),
+			// ManageFleet:      service.NewManageFleetService(fleetGen, fleetRepo, txm, psm),
+			// AssignFleet:      service.NewAssignFleetService(fleetGen, fleetRepo, txm),
+		},
+	}
+}
