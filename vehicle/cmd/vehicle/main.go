f9cec4466c25d268f5371f25c4f3ac60935d7cac
tomofiles <tomofiles.ssd.1@gmail.com>
2021-06-19 15:12:18 +0900

    vehicleのmain関数を実装

--- vehicle/cmd/vehicle/main.go
+++ vehicle/cmd/vehicle/main.go
@@ -1,102 +1,80 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"net"
+	"time"
+	proto "vehicle/pkg/skysign_proto"
+	"vehicle/pkg/vehicle/adapters/postgresql"
+	"vehicle/pkg/vehicle/adapters/rabbitmq"
+	"vehicle/pkg/vehicle/app"
+	"vehicle/pkg/vehicle/ports"
 
 	"github.com/golang/glog"
+	"google.golang.org/grpc"
 )
 
 var (
 	port *string
 )
 
-// func run() error {
-// 	ctx := context.Background()
-// 	ctx, cancel := context.WithCancel(ctx)
-// 	defer cancel()
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-// 	listen, err := net.Listen("tcp", ":"+*port)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer listen.Close()
-// 	s := grpc.NewServer(grpc.UnaryInterceptor(ports.LogBodyInterceptor()))
+	listen, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+	s := grpc.NewServer(grpc.UnaryInterceptor(ports.LogBodyInterceptor()))
 
-// 	db, err := postgresql.NewPostgresqlConnection()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	txm := postgresql.NewGormTransactionManager(db)
+	db, err := postgresql.NewPostgresqlConnection()
+	if err != nil {
+		return err
+	}
+	txm := postgresql.NewGormTransactionManager(db)
 
-// 	conn, err := rabbitmq.NewRabbitMQConnection()
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer conn.Close()
-// 	psm := rabbitmq.NewPubSubManager(conn)
+	conn, err := rabbitmq.NewRabbitMQConnection()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	psm := rabbitmq.NewPubSubManager(conn)
 
-// 	application := app.NewApplication(ctx, txm, psm)
+	application := app.NewApplication(ctx, txm, psm)
 
-// 	svc := ports.NewGrpcServer(application)
-// 	evt := ports.NewEventHandler(application)
+	svc := ports.NewGrpcServer(application)
+	evt := ports.NewEventHandler(application)
 
-// 	psm.SetConsumer(
-// 		ctx,
-// 		ports.FlightplanCreatedEventExchangeName,
-// 		ports.FlightplanCreatedEventQueueName,
-// 		func(event []byte) {
-// 			if err := evt.HandleCreatedEvent(ctx, event); err != nil {
-// 				glog.Error(err)
-// 			}
-// 		},
-// 	)
-// 	psm.SetConsumer(
-// 		ctx,
-// 		ports.FlightplanDeletedEventExchangeName,
-// 		ports.FlightplanDeletedEventQueueName,
-// 		func(event []byte) {
-// 			if err := evt.HandleDeletedEvent(ctx, event); err != nil {
-// 				glog.Error(err)
-// 			}
-// 		},
-// 	)
-// 	psm.SetConsumer(
-// 		ctx,
-// 		ports.FlightplanCopiedEventExchangeName,
-// 		ports.FlightplanCopiedEventQueueName,
-// 		func(event []byte) {
-// 			if err := evt.HandleCopiedEvent(ctx, event); err != nil {
-// 				glog.Error(err)
-// 			}
-// 		},
-// 	)
-// 	psm.SetConsumer(
-// 		ctx,
-// 		ports.FlightplanCopiedWhenFlightoperationCreatedEventExchangeName,
-// 		ports.FlightplanCopiedWhenFlightoperationCreatedEventQueueName,
-// 		func(event []byte) {
-// 			if err := evt.HandleCopiedWhenFlightoperationCreatedEvent(ctx, event); err != nil {
-// 				glog.Error(err)
-// 			}
-// 		},
-// 	)
+	psm.SetConsumer(
+		ctx,
+		ports.VehicleCopiedWhenFlightplanCopiedEventExchangeName,
+		ports.VehicleCopiedWhenFlightplanCopiedEventQueueName,
+		func(event []byte) {
+			if err := evt.HandleVehicleCopiedWhenFlightplanCopiedEvent(ctx, event); err != nil {
+				glog.Error(err)
+			}
+		},
+	)
 
-// 	proto.RegisterManageFlightplanServiceServer(s, &svc)
-// 	proto.RegisterAssignAssetsToFlightplanServiceServer(s, &svc)
+	proto.RegisterManageVehicleServiceServer(s, &svc)
 
-// 	glog.Info("start flightplan server")
-// 	return s.Serve(listen)
-// }
+	glog.Info("start vehicle server")
+	return s.Serve(listen)
+}
 
 func main() {
-	port = flag.String("port", "5001", "flightplan port")
+	port = flag.String("port", "5001", "vehicle port")
 	flag.Parse()
 	defer glog.Flush()
 
-	// for {
-	// 	if err := run(); err != nil {
-	// 		glog.Error(err)
-	// 		time.Sleep(10 * time.Second)
-	// 	}
-	// }
+	for {
+		if err := run(); err != nil {
+			glog.Error(err)
+			time.Sleep(10 * time.Second)
+		}
+	}
 }
