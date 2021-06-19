b62f76a31a64198e455e903b74362468c1f4b509
tomofiles <tomofiles.ssd.1@gmail.com>
2021-06-19 15:37:04 +0900

    vehicleのjava実装を削除

--- vehicle_java/src/main/java/net/tomofiles/skysign/vehicle/service/dpo/GetVehicleResponseDpo.java
+++
@@ -1,7 +0,0 @@
-package net.tomofiles.skysign.vehicle.service.dpo;
-
-import net.tomofiles.skysign.vehicle.domain.vehicle.Vehicle;
-
-public interface GetVehicleResponseDpo {
-    public void setVehicle(Vehicle vehicle);
-}
