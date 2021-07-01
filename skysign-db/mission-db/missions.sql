e023708b725df747de1386e497107902de4e1da7
tomofiles <tomofiles.ssd.1@gmail.com>
2021-07-01 23:39:44 +0900

    missionのデータベース定義を修正

--- skysign-db/mission-db/missions.sql
+++ skysign-db/mission-db/missions.sql
@@ -1,7 +1,6 @@
 CREATE TABLE missions (
     id character varying(36) NOT NULL,
     name character varying(200) NOT NULL,
-    takeoff_point_ground_height_wgs84_ellipsoid_m double precision NOT NULL,
     is_carbon_copy boolean NOT NULL,
     version character varying(36) NOT NULL,
     CONSTRAINT missions_pkey PRIMARY KEY (id)
