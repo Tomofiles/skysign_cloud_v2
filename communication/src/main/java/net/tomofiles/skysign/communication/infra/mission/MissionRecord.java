12902b3e6289015069d3325c2bdaf66a25a2cb45
tomofiles <tomofiles.ssd.1@gmail.com>
2021-07-05 15:29:15 +0900

    communicationにuploadmission機能を追加

---
+++ communication/src/main/java/net/tomofiles/skysign/communication/infra/mission/MissionRecord.java
@@ -0,0 +1,12 @@
+package net.tomofiles.skysign.communication.infra.mission;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissionRecord {
+    private String id;
+}
