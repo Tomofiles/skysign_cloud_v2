28d4e97492fb552898397785536b22689069b6bf
tomofiles <tomofiles.ssd.1@gmail.com>
2021-08-30 19:05:49 +0900

    古いcommunicationプロジェクトを削除

--- communication_bk/src/main/java/net/tomofiles/skysign/communication/domain/communication/Generator.java
+++
@@ -1,8 +0,0 @@
-package net.tomofiles.skysign.communication.domain.communication;
-
-import java.time.LocalDateTime;
-
-public interface Generator {
-    public CommandId newCommandId();
-    public LocalDateTime newTime();
-}
