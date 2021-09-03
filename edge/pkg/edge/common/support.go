b6c4f9ffd68283ca5d4a62006c698a11f609bc2c
tomofiles <tomofiles.ssd.1@gmail.com>
2021-09-03 19:43:34 +0900

    edgeのsupportのログ出力方式を見直し

--- edge/pkg/edge/common/support.go
+++ edge/pkg/edge/common/support.go
@@ -10,15 +10,23 @@ type Support interface {
 
 // NewSupport .
 func NewSupport() Support {
-	return &support{}
+	return &supportLog{}
 }
 
-type support struct{}
+type supportLog struct{}
 
-func (s *support) NotifyInfo(format string, args ...interface{}) {
-	glog.Infof(format, args)
+func (s *supportLog) NotifyInfo(format string, args ...interface{}) {
+	if len(args) != 0 {
+		glog.Infof(format, args)
+	} else {
+		glog.Info(format)
+	}
 }
 
-func (s *support) NotifyError(format string, args ...interface{}) {
-	glog.Errorf(format, args)
+func (s *supportLog) NotifyError(format string, args ...interface{}) {
+	if len(args) != 0 {
+		glog.Errorf(format, args)
+	} else {
+		glog.Errorf(format)
+	}
 }
