6d6e315e30a2a870934215e69597597317f25e01
tomofiles <tomofiles.ssd.1@gmail.com>
2021-09-14 19:14:25 +0900

    edgeのエラー時の挙動を整理＆os.Exit(1)でエラー終了させる＆docker-composeの再起動フラグを調整

--- edge-px4/cmd/edge-px4/main.go
+++ edge-px4/cmd/edge-px4/main.go
@@ -10,9 +10,11 @@ import (
 	mavlink_command "edge-px4/pkg/edge/domain/mavlink/command"
 	mavlink_telemetry "edge-px4/pkg/edge/domain/mavlink/telemetry"
 	"edge-px4/pkg/edge/domain/model"
+	"errors"
 	"flag"
 	"os"
 	"os/signal"
+	"sync"
 	"time"
 )
 
@@ -21,7 +23,7 @@ var (
 	cloud  = "http://localhost:8080"
 )
 
-func main() {
+func run() error {
 	flag.Parse()
 	defer glog_adapter.Flush()
 
@@ -38,118 +40,167 @@ func main() {
 	support := glog_adapter.NewSupport()
 	ticker := time_adapter.NewTicker(500 * time.Millisecond)
 
-	go func() {
-		for {
-			t := time.NewTimer(1 * time.Second)
-			select {
-			case <-t.C:
-				ctx := context.Background()
-				ctx, cancel := context.WithCancel(ctx)
-
-				gr, err := grpc_adapter.NewGrpcClientConnectionWithBlock(mavsdk)
-				if err != nil {
-					support.NotifyError("grpc client connection error: %v", err)
-					continue
-				}
-
-				telemetryStream, err := builder.MavlinkTelemetry(ctx, gr, support)
-				if err != nil {
-					support.NotifyError("mavlink telemetry error: %v", err)
-					cancel()
-					continue
-				}
-
-				telemetry := model.NewTelemetry()
-				updateExit := mavlink_telemetry.Updater(
-					ctx,
-					support,
-					telemetry,
-					telemetryStream.ConnectionStateStream,
-					telemetryStream.PositionStream,
-					telemetryStream.QuaternionStream,
-					telemetryStream.VelocityStream,
-					telemetryStream.ArmedStream,
-					telemetryStream.FlightModeStream,
-				)
-
-				cloudlinkAdapters := builder.Cloudlink(
-					ctx,
-					cloud,
-					support,
-					telemetry,
-				)
-
-				commandStream := cloudlink.CloudTicker(
-					ctx,
-					support,
-					ticker,
-					cloudlinkAdapters.PushTelemetry,
-					cloudlinkAdapters.PullCommand,
-					cloudlinkAdapters.PullUploadMission,
-					cloudlinkAdapters.GetUploadMission,
-				)
-
-				cStream := mavlink_command.CommandDistributer(ctx, support, commandStream.CommandStream)
-				mStream := mavlink_command.MissionDistributer(ctx, support, commandStream.MissionStream)
-
-				mavlinkAdapters := builder.MavlinkCommand(ctx, gr)
-
-				armSendExit := mavlink_command.CommandSender(ctx, support, cStream.ArmStream, mavlinkAdapters.AdapterArm, "ARM")
-				disarmSendExit := mavlink_command.CommandSender(ctx, support, cStream.DisarmStream, mavlinkAdapters.AdapterDisarm, "DISARM")
-				startSendExit := mavlink_command.CommandSender(ctx, support, cStream.StartStream, mavlinkAdapters.AdapterStart, "START")
-				pauseSendExit := mavlink_command.CommandSender(ctx, support, cStream.PauseStream, mavlinkAdapters.AdapterPause, "PAUSE")
-				takeoffSendExit := mavlink_command.CommandSender(ctx, support, cStream.TakeoffStream, mavlinkAdapters.AdapterTakeoff, "TAKEOFF")
-				landSendExit := mavlink_command.CommandSender(ctx, support, cStream.LandStream, mavlinkAdapters.AdapterLand, "LAND")
-				returnSendExit := mavlink_command.CommandSender(ctx, support, cStream.ReturnStream, mavlinkAdapters.AdapterReturn, "RETURN")
-				uploadSendExit := mavlink_command.MissionSender(ctx, support, mStream, mavlinkAdapters.AdapterUpload)
-
-				// // 障害時動作確認用
-				// go func() {
-				// 	t := time.NewTimer(5 * time.Second)
-				// 	select {
-				// 	case <-t.C:
-				// 		cancel()
-				// 	}
-				// }()
-
-				func() {
-					defer func() {
-						support.NotifyInfo("main loop exit")
-						cancel()
-					}()
-					for {
-						select {
-						case <-updateExit:
-							return
-						case <-armSendExit:
-							return
-						case <-disarmSendExit:
-							return
-						case <-startSendExit:
-							return
-						case <-pauseSendExit:
-							return
-						case <-takeoffSendExit:
-							return
-						case <-landSendExit:
-							return
-						case <-returnSendExit:
-							return
-						case <-uploadSendExit:
-							return
-						}
-					}
-				}()
-			}
-		}
-	}()
+	support.NotifyInfo("edge PX4 start")
 
 	stop := make(chan os.Signal)
 	signal.Notify(stop, os.Interrupt)
 
-	<-stop
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
 
-	time.Sleep(1 * time.Second)
+	gr, err := grpc_adapter.NewGrpcClientConnectionWithBlockAndTimeout(mavsdk)
+	if err != nil {
+		support.NotifyError("grpc client connection error: %v", err)
+		return err
+	}
 
-	defer support.NotifyInfo("edge PX4 end")
+	telemetryStream, err := builder.MavlinkTelemetry(ctx, gr, support)
+	if err != nil {
+		support.NotifyError("mavlink telemetry error: %v", err)
+		return err
+	}
+
+	telemetry := model.NewTelemetry()
+
+	connectionStateExit := mavlink_telemetry.ConnectionStateUpdater(ctx, support, telemetry, telemetryStream.ConnectionStateStream)
+	positionExit := mavlink_telemetry.PositionUpdater(ctx, support, telemetry, telemetryStream.PositionStream)
+	quaternionExit := mavlink_telemetry.QuaternionUpdater(ctx, support, telemetry, telemetryStream.QuaternionStream)
+	velocityExit := mavlink_telemetry.VelocityUpdater(ctx, support, telemetry, telemetryStream.VelocityStream)
+	armedExit := mavlink_telemetry.ArmedUpdater(ctx, support, telemetry, telemetryStream.ArmedStream)
+	flightModeExit := mavlink_telemetry.FlightModeUpdater(ctx, support, telemetry, telemetryStream.FlightModeStream)
+
+	cloudlinkAdapters := builder.Cloudlink(
+		ctx,
+		cloud,
+		support,
+		telemetry,
+	)
+
+	commandStream := cloudlink.CloudTicker(
+		ctx,
+		support,
+		ticker,
+		cloudlinkAdapters.PushTelemetry,
+		cloudlinkAdapters.PullCommand,
+		cloudlinkAdapters.PullUploadMission,
+		cloudlinkAdapters.GetUploadMission,
+	)
+
+	cStream := mavlink_command.CommandDistributer(ctx, support, commandStream.CommandStream)
+	mStream := mavlink_command.MissionDistributer(ctx, support, commandStream.MissionStream)
+
+	mavlinkAdapters := builder.MavlinkCommand(ctx, gr)
+
+	armSendExit := mavlink_command.CommandSender(ctx, support, cStream.ArmStream, mavlinkAdapters.AdapterArm, "ARM")
+	disarmSendExit := mavlink_command.CommandSender(ctx, support, cStream.DisarmStream, mavlinkAdapters.AdapterDisarm, "DISARM")
+	startSendExit := mavlink_command.CommandSender(ctx, support, cStream.StartStream, mavlinkAdapters.AdapterStart, "START")
+	pauseSendExit := mavlink_command.CommandSender(ctx, support, cStream.PauseStream, mavlinkAdapters.AdapterPause, "PAUSE")
+	takeoffSendExit := mavlink_command.CommandSender(ctx, support, cStream.TakeoffStream, mavlinkAdapters.AdapterTakeoff, "TAKEOFF")
+	landSendExit := mavlink_command.CommandSender(ctx, support, cStream.LandStream, mavlinkAdapters.AdapterLand, "LAND")
+	returnSendExit := mavlink_command.CommandSender(ctx, support, cStream.ReturnStream, mavlinkAdapters.AdapterReturn, "RETURN")
+	uploadSendExit := mavlink_command.MissionSender(ctx, support, mStream, mavlinkAdapters.AdapterUpload)
+
+	exitWaiter := func() <-chan struct{} {
+		exits := []<-chan struct{}{
+			connectionStateExit,
+			positionExit,
+			quaternionExit,
+			velocityExit,
+			armedExit,
+			flightModeExit,
+			armSendExit,
+			disarmSendExit,
+			startSendExit,
+			pauseSendExit,
+			takeoffSendExit,
+			landSendExit,
+			returnSendExit,
+			uploadSendExit,
+		}
+
+		stream := make(chan struct{})
+
+		go func() {
+			defer close(stream)
+			var wg sync.WaitGroup
+			for _, exit := range exits {
+				wg.Add(1)
+				go func(exit <-chan struct{}) {
+					<-exit
+					wg.Done()
+				}(exit)
+			}
+			wg.Wait()
+		}()
+
+		return stream
+	}()
+
+	return func() (err error) {
+		defer func() {
+			cancel()
+			<-exitWaiter
+			if err != nil {
+				support.NotifyError("edge PX4 error: %v", err)
+				return
+			}
+			support.NotifyInfo("edge PX4 end")
+		}()
+		for {
+			select {
+			case <-stop:
+				err = nil
+				return
+			case <-connectionStateExit:
+				err = errors.New("connectionStateExit")
+				return
+			case <-positionExit:
+				err = errors.New("positionExit")
+				return
+			case <-quaternionExit:
+				err = errors.New("quaternionExit")
+				return
+			case <-velocityExit:
+				err = errors.New("velocityExit")
+				return
+			case <-armedExit:
+				err = errors.New("armedExit")
+				return
+			case <-flightModeExit:
+				err = errors.New("flightModeExit")
+				return
+			case <-armSendExit:
+				err = errors.New("armSendExit")
+				return
+			case <-disarmSendExit:
+				err = errors.New("disarmSendExit")
+				return
+			case <-startSendExit:
+				err = errors.New("startSendExit")
+				return
+			case <-pauseSendExit:
+				err = errors.New("pauseSendExit")
+				return
+			case <-takeoffSendExit:
+				err = errors.New("takeoffSendExit")
+				return
+			case <-landSendExit:
+				err = errors.New("landSendExit")
+				return
+			case <-returnSendExit:
+				err = errors.New("returnSendExit")
+				return
+			case <-uploadSendExit:
+				err = errors.New("uploadSendExit")
+				return
+			}
+		}
+	}()
+}
+
+func main() {
+	if err := run(); err != nil {
+		os.Exit(1)
+	}
 }
--- edge-px4/pkg/edge/domain/mavlink/telemetry/updater.go
+++ edge-px4/pkg/edge/domain/mavlink/telemetry/updater.go
@@ -6,60 +6,170 @@ import (
 	"edge-px4/pkg/edge/domain/model"
 )
 
-// Updater .
-func Updater(
+// ConnectionStateUpdater .
+func ConnectionStateUpdater(
 	ctx context.Context,
 	support common.Support,
 	telemetry model.Telemetry,
 	connectionStateStream <-chan *model.ConnectionState,
-	positionStream <-chan *model.Position,
-	quaternionStream <-chan *model.Quaternion,
-	velocityStream <-chan *model.Velocity,
-	armedStream <-chan *model.Armed,
-	flightModeStream <-chan *model.FlightMode,
 ) <-chan struct{} {
-	updateExit := make(chan struct{})
+	updaterExit := make(chan struct{})
 
 	go func() {
-		defer close(updateExit)
+		defer close(updaterExit)
 		for {
 			select {
 			case <-ctx.Done():
-				support.NotifyInfo("telemetry updater done")
+				support.NotifyInfo("telemetry CONNECTION_STATE done")
 				return
 			case connectionState, ok := <-connectionStateStream:
 				if !ok {
-					support.NotifyInfo("connectionStateStream close")
+					support.NotifyInfo("telemetry CONNECTION_STATE close")
 					return
 				}
 				telemetry.SetConnectionState(connectionState)
+			}
+		}
+	}()
+
+	return updaterExit
+}
+
+// PositionUpdater .
+func PositionUpdater(
+	ctx context.Context,
+	support common.Support,
+	telemetry model.Telemetry,
+	positionStream <-chan *model.Position,
+) <-chan struct{} {
+	updaterExit := make(chan struct{})
+
+	go func() {
+		defer close(updaterExit)
+		for {
+			select {
+			case <-ctx.Done():
+				support.NotifyInfo("telemetry POSITION done")
+				return
 			case position, ok := <-positionStream:
 				if !ok {
-					support.NotifyInfo("positionStream close")
+					support.NotifyInfo("telemetry POSITION close")
 					return
 				}
 				telemetry.SetPosition(position)
+			}
+		}
+	}()
+
+	return updaterExit
+}
+
+// QuaternionUpdater .
+func QuaternionUpdater(
+	ctx context.Context,
+	support common.Support,
+	telemetry model.Telemetry,
+	quaternionStream <-chan *model.Quaternion,
+) <-chan struct{} {
+	updaterExit := make(chan struct{})
+
+	go func() {
+		defer close(updaterExit)
+		for {
+			select {
+			case <-ctx.Done():
+				support.NotifyInfo("telemetry QUATERNION done")
+				return
 			case quaternion, ok := <-quaternionStream:
 				if !ok {
-					support.NotifyInfo("quaternionStream close")
+					support.NotifyInfo("telemetry QUATERNION close")
 					return
 				}
 				telemetry.SetQuaternion(quaternion)
+			}
+		}
+	}()
+
+	return updaterExit
+}
+
+// VelocityUpdater .
+func VelocityUpdater(
+	ctx context.Context,
+	support common.Support,
+	telemetry model.Telemetry,
+	velocityStream <-chan *model.Velocity,
+) <-chan struct{} {
+	updaterExit := make(chan struct{})
+
+	go func() {
+		defer close(updaterExit)
+		for {
+			select {
+			case <-ctx.Done():
+				support.NotifyInfo("telemetry VELOCITY done")
+				return
 			case velocity, ok := <-velocityStream:
 				if !ok {
-					support.NotifyInfo("velocityStream close")
+					support.NotifyInfo("telemetry VELOCITY close")
 					return
 				}
 				telemetry.SetVelocity(velocity)
+			}
+		}
+	}()
+
+	return updaterExit
+}
+
+// ArmedUpdater .
+func ArmedUpdater(
+	ctx context.Context,
+	support common.Support,
+	telemetry model.Telemetry,
+	armedStream <-chan *model.Armed,
+) <-chan struct{} {
+	updaterExit := make(chan struct{})
+
+	go func() {
+		defer close(updaterExit)
+		for {
+			select {
+			case <-ctx.Done():
+				support.NotifyInfo("telemetry ARMED done")
+				return
 			case armed, ok := <-armedStream:
 				if !ok {
-					support.NotifyInfo("armedStream close")
+					support.NotifyInfo("telemetry ARMED close")
 					return
 				}
 				telemetry.SetArmed(armed)
+			}
+		}
+	}()
+
+	return updaterExit
+}
+
+// FlightModeUpdater .
+func FlightModeUpdater(
+	ctx context.Context,
+	support common.Support,
+	telemetry model.Telemetry,
+	flightModeStream <-chan *model.FlightMode,
+) <-chan struct{} {
+	updaterExit := make(chan struct{})
+
+	go func() {
+		defer close(updaterExit)
+		for {
+			select {
+			case <-ctx.Done():
+				support.NotifyInfo("telemetry FLIGHT_MODE done")
+				return
 			case flightMode, ok := <-flightModeStream:
 				if !ok {
-					support.NotifyInfo("flightModeStream close")
+					support.NotifyInfo("telemetry FLIGHT_MODE close")
 					return
 				}
 				telemetry.SetFlightMode(flightMode)
@@ -67,5 +177,5 @@ func Updater(
 		}
 	}()
 
-	return updateExit
+	return updaterExit
 }
